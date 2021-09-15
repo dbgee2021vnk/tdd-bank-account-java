f11984acd20063bf6948362cab5794d11229b949
vnk <dbgee2021vnk@gmail.com>
2021-09-15 17:28:14 +0530

    Example for Commit

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,8 @@
 package org.xpdojo.bank;
 
 public class Account {
+    public int balance;
+    public void deposit(int 1){
+        balance+=1;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,5 +1,7 @@
 package org.xpdojo.bank;
 
+import org.junit.Test;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -8,8 +10,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    //@Disabled
+    //public void depositAnAmountToIncreaseTheBalance() {
+    //  assertThat("your first test").isBlank();
+    //arrange
+    //Account account=new Account();
+    //account.deposit(100);
+    //assertThat(account.balance,is(100));
+    public void startingBalanceIsZero
+
+    {
+        Account account = new Account();
+        assertThat(account.balance, is(0));
+
     }
+
+    @Test
+    public void depositAnAmountToIncreaseTheBalance() {
+        //   assertThat("your first test").isBlank();
+        Account account = new Account();
+        account.deposit(100);
+        assertThat(account.balance, is(100));
+    }
+
+    @Test
+    public void depositMultipleAmounts() {
+        //   assertThat("your first test").isBlank();
+        Account account = new Account();
+        account.deposit(100);
+        account.deposit(300);
+        assertThat(account.balance, is(400));
+    }
+
 }
